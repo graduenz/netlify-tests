e02a7542746f0d47f142c2bbcb15e2aa869ed20f
Guilherme Augusto Raduenz <graduenz@gmail.com>
2020-07-14 07:13:37 -0300

    changes in protected function

--- lambda/protected/protected.go
+++ lambda/protected/protected.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-lambda-go/lambdacontext"
+	"github.com/netlify/gotrue/models"
 )
+
+type netlify struct {
+	user models.User `json:"user",omitempty`
+}
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	lc, ok := lambdacontext.FromContext(ctx)
@@ -20,18 +27,20 @@ func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*event
 
 	cc := lc.ClientContext
 
-	fmt.Println("client")
-	fmt.Println(cc.Client)
+	netlifyJSON, _ := base64.StdEncoding.DecodeString(cc.Custom["netlify"])
+	netlify := netlify{}
+	netlifyUser := json.Unmarshal([]byte(netlifyJson), &netlify)
 
-	fmt.Println("env")
-	fmt.Println(cc.Env)
-
-	fmt.Println("custom")
-	fmt.Println(cc.Custom)
+	if netlify.user == nil {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Don't know you bro :/",
+		}, nil
+	}
 
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello, wait",
+		Body:       fmt.Sprintf("Hello, %s", netlify.user.Email),
 	}, nil
 }
 
